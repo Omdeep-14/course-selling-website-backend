764019e8513be191cf2de231a88cbb88dfe4f57c
Omdeep-14 <202654484+Omdeep-14@users.noreply.github.com.>
2025-04-27 18:16:40 +0530

    bugs in routes fixed

--- routes/course.js
+++ routes/course.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const courseRouter = Router();
 
-courseRouter.get("/courses/preview", function (req, res) {});
+courseRouter.get("/preview", function (req, res) {});
 
-courseRouter.post("/courses/purchase", function (req, res) {});
+courseRouter.post("/purchase", function (req, res) {});
 
 module.export = {
   courseRouter: courseRouter,
